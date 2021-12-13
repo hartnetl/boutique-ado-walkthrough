054ea9e666ae70eabc3d4836dd70612abb51a8ba
hartnetl <laura.hartnett@outlook.com>
2021-12-13 23:42:51 +0000

    Feat: Add logging to settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,6 +30,23 @@ DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['ci-boutique-ado-walkthrough.herokuapp.com', 'localhost']
 
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
